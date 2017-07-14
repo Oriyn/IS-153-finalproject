db9657c3cfc2b07c1473e9551594e5291a7b515f
Gabe Haggard <gabehagg@gmail.com>
2017-07-14 14:01:10 -0500

    added Classes to the game

--- Dungeoneers/Program.cs
+++ Dungeoneers/Program.cs
@@ -4,17 +4,21 @@ namespace Dungeoneers
 {
     class MainClass
     {
-        //public static float timer = 1.0f;
+        public static int score = 0;
         public static string name;
         public static int proceed;
 		public static int health = 20;
         public static int damage = 20;
+        public static string Class; 
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome brave soul to Dungeoneers!");
             Console.WriteLine("What is your name Taveler?: ");
             name = Console.ReadLine();
-            Console.WriteLine("Well then " + name + " Which dungeon do you choose to enter? 1, 2, or 3");
+            Console.WriteLine("Choose a class " + name);
+            Console.WriteLine("Cleric, Paladin, Wizard, Bard, Warrior, Thief: ");
+            Class = Console.ReadLine();
+            Console.WriteLine("Well then " + name + " the " + Class + " Which dungeon do you choose to enter? 1, 2, or 3");
             string answer = Console.ReadLine();
 
             int dungeon = int.Parse(answer);
@@ -58,12 +62,22 @@ namespace Dungeoneers
                 else if (getChest.Contains("no"))
                 {
                     Console.WriteLine("Well then you will never know what could have been in that chest");
+                    score += 10;
                 }
 
             }
             else if (proceed == 2)
             {
+                Console.WriteLine("you see a light off in the distance");
+                Console.WriteLine("as you get closer you notice a green liquid in a green vial"); 
+                Console.WriteLine("the vial look quite delicious, do you choose to drink it?: ");
+                string drink = Console.ReadLine();
 
+                if (drink.Contains("yes"))
+                {
+                    Console.WriteLine("You feel your body pulse with energy gaining 10 points !!");
+                    score += 10;
+                }
             }
             else
             {
@@ -72,8 +86,8 @@ namespace Dungeoneers
 
             Console.WriteLine("As you move on down a long hallway");
             Console.WriteLine("It seems to never end");
-            Console.WriteLine("but lo and behold you are faced with 2 doors, one marked door 1 and the other door 2");
-            Console.WriteLine("which door do you choose" + name + "?: ");
+            Console.WriteLine("but lo and behold you are faced with 2 doors, one marked Left Door and the other Right Door");
+            Console.WriteLine("which door do you choose " + name + "?: ");
             string doorchoice = Console.ReadLine();
 
             if (doorchoice.Contains("door 1"))
@@ -102,17 +116,17 @@ namespace Dungeoneers
             {
                string decide1 = Console.ReadLine();
 
-                if (decide1 == "left")
+                if (decide1.Contains("left"))
                 {
                     Console.WriteLine("you proceed left");
                     proceed += 1;
                 }
-                else if (decide1 == "right")
+                else if (decide1.Contains("right"))
                 {
                     Console.WriteLine("you proceed right");
                     proceed += 2;
                 }
-                else if (decide1 == "straight")
+                else if (decide1.Contains("straight"))
                 {
                   Console.WriteLine("you proceed straight");
                     proceed += 3;
@@ -131,6 +145,8 @@ namespace Dungeoneers
             if (health == 0)
             {
                 Console.WriteLine("You have perished, Better luck next time adventurer");
+                Console.WriteLine(name +" the " +Class+ " scored " + score + " points !");
+                Console.ReadLine();
                 Environment.Exit(0);
             }
             else
